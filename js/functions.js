01c40af50ba153b47f86f562004c51217b09da52
AaroSoranko <asoranko@gmail.com>
2024-11-07 10:29:47 +0200

    muutoksia jotta arvauslaskuri toimii

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-'use script'
+'use strict'
 
 const input = document.querySelector('input')
 const output = document.querySelector('output')
@@ -17,13 +17,15 @@ const words = [
     "kauppa"
 ]
 
-let randomizedWord =''
-let maskedWord =''
+let randomizedWord = ''
+let maskedWord = ''
+let guessCount = 0
 
 const newGame = () => {
-    const random = Math.floor(Math.random() * 10) + 1
+    const random = Math.floor(Math.random() * words.length)
     randomizedWord = words[random]
     maskedWord = "*".repeat(randomizedWord.length)
+    guessCount = 0
     console.log(randomizedWord)
     output.innerHTML = maskedWord
 }
@@ -33,14 +35,13 @@ const win = () => {
     newGame()
 }
 
-const replaceFoundChars = (guess) =>{
-    for (let i = 0;i<randomizedWord.length;i++) {
-        const char = randomizedWord.substring(i,i+1)
+const replaceFoundChars = (guess) => {
+    for (let i = 0; i < randomizedWord.length; i++) {
+        const char = randomizedWord.substring(i, i + 1)
         if (char === guess) {
             let newString = maskedWord.split('')
-            newString.splice(i,1,guess)
-            newString = newString.join('')
-            maskedWord = newString
+            newString.splice(i, 1, guess)
+            maskedWord = newString.join('')
         }
     }
     output.innerHTML = maskedWord
@@ -48,21 +49,23 @@ const replaceFoundChars = (guess) =>{
 
 newGame()
 
-input.addEventListener('keypress',(e) => {
+input.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         e.preventDefault()
 
-        const guess =input.value
-        if (guess.toLowerCase()===randomizedWord.toLowerCase()){
-
-        } else if (guess.lenght === 1){
+        const guess = input.value
+        guessCount++
+        span.textContent = guessCount
+        if (guess.toLowerCase() === randomizedWord.toLowerCase()) {
+            win()
+        } else if (guess.length === 1) {
             replaceFoundChars(guess)
-            if (maskedWord.toLocaleLowerCase() = randomizedWord.toLocaleLowerCase()){
+            if (maskedWord.toLowerCase() === randomizedWord.toLowerCase()) {
                 win()
             }
         } else {
             alert("Arvasit väärin hahaa")
         }
-        input.value=''
+        input.value = ''
     }
 })
